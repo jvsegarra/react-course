64c0ca485a4174dd08cf08025f02a4b7117d510e
jvsegarra <jvsegarra@gmail.com>
2016-10-16 23:55:55 +0200

    exercise 3 - structure for bigger projects

---
+++ 1. Basic redux/Exercise02-Refactoring-Exercise03/v06-exercise03-structure for a bigger project/reducers/index.js
@@ -0,0 +1,47 @@
+// step 2.2
+function counter(currentState, action) {
+    var DEFAULT_STATE = 0;
+    if (currentState === undefined) {
+        nextState = DEFAULT_STATE;
+        return nextState;
+    }
+
+    switch (action.type) {
+        case 'DECREMENT':
+            nextState = currentState - 1;
+            return nextState;
+        case 'INCREMENT':
+            nextState = currentState + 1;
+            return nextState;
+        default:
+            nextState = currentState;
+            return nextState;
+    }
+}
+
+function sum(currentState, action){
+    var DEFAULT_STATE = 3;
+    if (currentState === undefined) {
+        nextState = DEFAULT_STATE;
+        return nextState;
+    }
+
+    switch (action.type) {
+      case 'SUM':
+        nextState = parseInt(action.a) + parseInt(action.b);
+        return nextState;
+      default:
+        nextState = currentState;
+        return nextState;
+    }
+}
+
+function combineReducer(currentState, action) {
+    var nextState = Object.assign({},currentState);
+    nextState = {
+        count:counter(nextState.count, action),
+        sum: sum(nextState.sum, action)
+    }
+
+    return nextState;
+}
